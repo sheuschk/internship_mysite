5e617a4ed90e396894fce936e4e7d7805e812229
sheuschk <58145416+sheuschk@users.noreply.github.com>
2019-11-25 18:11:16 +0000

    Update the redirect comment

--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,5 @@ from django.http import HttpResponseRedirect
 
 
 def redirect(request):
-    """Redirects everyone to the polls app index after starting this django app with the localhost.
-    It does not fit perfectly in the mysite directory --> find better solution"""
+    """Redirects everyone to the polls app index after starting this django app with the localhost."""
     return HttpResponseRedirect("/polls")
